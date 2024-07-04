5f9b565a9d42aaac72d11a7260dcebd36401f68a
JamesComo44 <21957020+JamesComo44@users.noreply.github.com>
2024-07-04 16:32:54 +0100

    Bunch of changes to pistats

--- programs/pistats.py
+++ programs/pistats.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+import re
 import signal
 import socket
 from contextlib import contextmanager
 from dataclasses import dataclass
 from pathlib import Path
-from threading import Barrier, BrokenBarrierError
+from subprocess import check_output
+from threading import Barrier, BrokenBarrierError, Timer
 
 import requests
-from pifacecad import IODIR_ON, PiFaceCAD, SwitchEventListener
+from pifacecad import IODIR_ON, LCDBitmap, PiFaceCAD, SwitchEventListener
+from pifacecad.tools.question import LCDQuestion
 
-from clear import clear_lcd, turn_on_lcd
+from clear import clear_lcd, turn_off_lcd, turn_on_lcd
 
 
 @dataclass
@@ -110,48 +113,235 @@ def uptime():
     return f"{days}D {hours}H {minutes}M"
 
 
+def run_shell(command) -> str:
+    return check_output(command, shell=True).decode("utf-8")
+
+
+def memory_usage_percent() -> str:
+    total_memory = run_shell("free | grep 'Mem' | awk '{print $2}'")
+    used_memory = run_shell("free | grep 'Mem' | awk '{print $3}'")
+
+    used_percentage = float(used_memory) / float(total_memory)
+    return f"{used_percentage:.1}%"
+
+
+def current_temperature() -> str:
+    return run_shell("/usr/bin/vcgencmd measure_temp | awk -F'=' '{print $2}'")
+
+
+def pihole_update_available() -> bool:
+    pattern = r"v(\d+\.\d+\.\d+)\s+\(Latest:\s+v(\d+\.\d+\.\d+)\)"
+    matches = re.findall(pattern, run_shell("pihole -v"))
+
+    for current, latest in matches:
+        if current != latest:
+            print(f"Pihole update available. Current: {current} - Latest {latest}")
+            return True
+
+    return False
+
+
 class StatusDriver:
     def __init__(self):
         self.cad = PiFaceCAD()
         init_lcd(self.cad)
 
-        self.stats = pihole_stats()
-        self.uptime = uptime()
-        self.render_status_ads_blocked()
+        self.memory_symbol_index = 0
+        self.cad.lcd.store_custom_bitmap(
+            self.memory_symbol_index,
+            LCDBitmap([0xE, 0x1F, 0xE, 0x1F, 0xE, 0x1F, 0xE, 0x0]),
+        )
+
+        self.temperature_symbol_index = 1
+        self.cad.lcd.store_custom_bitmap(
+            self.temperature_symbol_index,
+            LCDBitmap([0x4, 0xA, 0xA, 0xA, 0x11, 0x1F, 0xE, 0x0]),
+        )
+
+        self.ip_symbol_index = 2
+        self.cad.lcd.store_custom_bitmap(
+            self.ip_symbol_index,
+            LCDBitmap(
+                [0x1C, 0x8, 0x8, 0x1C, 0x7, 0x5, 0x7, 0x4],
+            ),
+        )
+
+        self._backlight_on = True
+        self._update_interval_min = 30
+
+        self.stats = ""
+        self.uptime = ""
+        self.memory = ""
+        self.temperature = ""
+
+        self.update()
+
+        self.update_timer = Timer(self._update_interval_min * 60, self.auto_update)
+        self.update_timer.start()
+
+        self.current_render_page_index = 0
+        self.render_status_ip()
+
+    def choose_function(self, event=None):
+        self.cancel_timers()
+
+        question = LCDQuestion(
+            question="Choose Function",
+            answers=["Backlight", "Interval", "Update Software", "Nothing"],
+            selector=">",
+            cad=self.cad,
+        )
+
+        answer_index = question.ask()
+
+        if answer_index == 0:
+            self.toggle_backlight()
+            self.auto_update()
+
+        if answer_index == 1:
+            self.update_interval()
+            return
+
+        if answer_index == 2:
+            self.update_software()
+            return
+
+        self.auto_update()
+
+    def update_software(self, event=None):
+        self.cancel_timers()
+
+        question = LCDQuestion(
+            question="Update Now?",
+            answers=["No", "Yes"],
+            selector="> ",
+            cad=self.cad,
+        )
+
+        should_update = question.ask()
+
+        if not should_update:
+            self.auto_update()
+            return
+
+        print("Updating Software...")
+
+        clear_lcd(self.cad)
+        toggle_cursor_line(self.cad, reset=True)
+        self.cad.lcd.write("Updating...")
+        toggle_cursor_line(self.cad)
+        self.cad.lcd.write("Do not unplug!")
+
+        run_shell("pihole -v")
+        # TODO: Run apt update
+
+        print("Updates Complete!")
+        self.auto_update()
 
     def update(self):
-        self.status = pihole_stats()
+        self.stats = pihole_stats()
         self.uptime = uptime()
+        self.memory = memory_usage_percent()
+        self.temperature = current_temperature()
 
-    def render_uptime_ip(self, event=None):
+        if pihole_update_available():
+            self.stats.status = "update!"
+
+    def auto_update(self):
+        if self._update_interval_min == 0:
+            return
+
+        print("Auto Updating")
+        self.update()
+
+        self.update_timer = Timer(self._update_interval_min * 60, self.auto_update)
+        self.update_timer.start()
+
+        if self.current_render_page_index == 0:
+            self.render_status_ip()
+        if self.current_render_page_index == 1:
+            self.render_uptime_memory_temp()
+        if self.current_render_page_index == 2:
+            self.render_blocked_info()
+        if self.current_render_page_index == 3:
+            self.render_client_info()
+
+    def cancel_timers(self):
+        if self.update_timer:
+            self.update_timer.cancel()
+
+    def update_interval(self, event=None):
+        self.cancel_timers()
+
+        intervals = ["1", "5", "10", "30", "60", "120", "240", "0"]
+
+        question = LCDQuestion(
+            question="Update Interval?",
+            answers=intervals,
+            selector="Minutes > ",
+            cad=self.cad,
+        )
+
+        answer_index = question.ask()
+        self._update_interval_min = int(intervals[answer_index])
+        self.auto_update()
+
+    def toggle_backlight(self, event=None):
+        if self._backlight_on:
+            self.cad.lcd.backlight_on()
+        else:
+            self.cad.lcd.backlight_off()
+
+        self._backlight_on = not self._backlight_on
+
+    def render_status_ip(self, event=None):
+        self.current_render_page_index = 0
+
         clear_lcd(self.cad)
         toggle_cursor_line(self.cad, reset=True)
-
-        self.cad.lcd.write(f"Up:{self.uptime}")
-        toggle_cursor_line(self.cad)
-        self.cad.lcd.write(f"IP:{self.stats.ip_address}")
-
-    def render_status_ads_blocked(self, event=None):
-        clear_lcd(self.cad)
-        toggle_cursor_line(self.cad, reset=True)
+        self.update()
 
         self.cad.lcd.write(f"Pihole {self.stats.status.title()}")
         toggle_cursor_line(self.cad)
+        self.cad.lcd.write_custom_bitmap(self.ip_symbol_index)
+        self.cad.lcd.write(f":{self.stats.ip_address}")
+
+    def render_uptime_memory_temp(self, event=None):
+        self.current_render_page_index = 1
+
+        clear_lcd(self.cad)
+        toggle_cursor_line(self.cad, reset=True)
+        self.update()
+
+        self.cad.lcd.write(f"Up:{self.uptime}")
+        toggle_cursor_line(self.cad)
+        self.cad.lcd.write_custom_bitmap(self.memory_symbol_index)
+        self.cad.lcd.write(self.memory)
+
+        self.cad.lcd.write(" ")
+
+        self.cad.lcd.write_custom_bitmap(self.temperature_symbol_index)
+        self.cad.lcd.write(self.temperature)
+
+    def render_blocked_info(self, event=None):
+        self.current_render_page_index = 2
+
+        clear_lcd(self.cad)
+        toggle_cursor_line(self.cad, reset=True)
+        self.update()
+
         self.cad.lcd.write(
             f"Ads:{self.stats.percentage_blocked:.1f}% {self.stats.ads_blocked}"
         )
-
-    def render_domains_blocked(self, event=None):
-        clear_lcd(self.cad)
-        toggle_cursor_line(self.cad, reset=True)
-
-        self.cad.lcd.write("Domains Blocked")
         toggle_cursor_line(self.cad)
-        self.cad.lcd.write(self.stats.domains_blocked)
+        self.cad.lcd.write(f"Sites:{self.stats.domains_blocked}")
 
     def render_client_info(self, event=None):
+        self.current_render_page_index = 3
+
         clear_lcd(self.cad)
         toggle_cursor_line(self.cad, reset=True)
+        self.update()
 
         self.cad.lcd.write(f"Clients: {self.stats.clients}")
         toggle_cursor_line(self.cad)
@@ -187,19 +377,18 @@ def signal_exit_handler(sig, frame):
 def main():
     print("Initializing Pihole Status...")
 
-    driver = StatusDriver()
-
     global end_barrier
     end_barrier = Barrier(2)
     signal.signal(signal.SIGINT, signal_exit_handler)
 
-    switchlistener = SwitchEventListener(chip=driver.cad)
-    switchlistener.register(0, IODIR_ON, driver.render_status_ads_blocked)
-    switchlistener.register(1, IODIR_ON, driver.render_domains_blocked)
-    switchlistener.register(2, IODIR_ON, driver.render_uptime_ip)
-    switchlistener.register(3, IODIR_ON, driver.render_client_info)
-    switchlistener.register(4, IODIR_ON, end_barrier.wait)
+    driver = StatusDriver()
 
+    switchlistener = SwitchEventListener(chip=driver.cad)
+    switchlistener.register(0, IODIR_ON, driver.render_status_ip)
+    switchlistener.register(1, IODIR_ON, driver.render_blocked_info)
+    switchlistener.register(2, IODIR_ON, driver.render_uptime_memory_temp)
+    switchlistener.register(3, IODIR_ON, driver.render_client_info)
+    switchlistener.register(4, IODIR_ON, driver.choose_function)
     switchlistener.activate()
 
     try:
@@ -210,7 +399,10 @@ def main():
     except SystemExit:
         print("Exiting Program")
     finally:
+        driver.cancel_timers()
         switchlistener.deactivate()
+        clear_lcd(driver.cad)
+        turn_off_lcd(driver.cad)
 
 
 if __name__ == "__main__":
